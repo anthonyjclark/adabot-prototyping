479e14250266d096593fe1eb4bf974c1b47c18e2
Lauren Tallerico <lat13@live.missouristate.edu>
2019-07-02 11:33:59 -0500

    fixed comment on line 19 regarding sleep cycle

--- rpi/demos/ledFade.py
+++ rpi/demos/ledFade.py
@@ -4,20 +4,20 @@ from time import sleep
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(32, GPIO.OUT)
-p = GPIO.PWM(32, 100) 	# starting frequency
-p.start(30) 		# starting duty cycle
+p = GPIO.PWM(32, 100)   # starting frequency
+p.start(30)         # starting duty cycle
 p.ChangeDutyCycle(80)
 p.ChangeFrequency(1000)
 duty_cycle = 0
-for count in range(40):			# loop for how many times you want program to run
-	if count % 2 == 0:		# every other time...
-		duty_cycle_inc = 5	# fade in LED
-	else:
-		duty_cycle_inc = -5	# fade out LED
-	for i in range(20):		# loop to change duty cycle and pause for viewing
-		p.ChangeDutyCycle(duty_cycle)
-		sleep(.05) 		# 4 seconds //doesn't sound right, recheck!
-		duty_cycle += duty_cycle_inc
+for count in range(40):     # loop for how many times you want program to run
+    if count % 2 == 0:      # every other time...
+        duty_cycle_inc = 5  # fade in LED
+    else:
+        duty_cycle_inc = -5 # fade out LED
+    for i in range(20):     # loop to change duty cycle and pause for viewing
+        p.ChangeDutyCycle(duty_cycle)
+        sleep(.05)          # 15 ms
+        duty_cycle += duty_cycle_inc
 input("Press return to stop: ")
 p.stop()
 GPIO.cleanup()
